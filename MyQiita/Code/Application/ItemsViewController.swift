c271cf52e04b2eac36e60cd13baa0308a53a0028
Naoto Kaneko <naoty.k@gmail.com>
2016-01-15 21:46:18 +0900

    Show items on UITableView

---
+++ MyQiita/Code/Application/ItemsViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ItemsViewController.swift
+//  MyQiita
+//
+//  Created by Naoto Kaneko on 1/15/16.
+//  Copyright © 2016 Naoto Kaneko. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+    var items: [Item] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        items.append(Item(title: "item 1"))
+        items.append(Item(title: "item 2"))
+        items.append(Item(title: "item 3"))
+        items.append(Item(title: "item 4"))
+        items.append(Item(title: "item 5"))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let item = items[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("ItemCell", forIndexPath: indexPath)
+        cell.textLabel?.text = item.title
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyQiita/Code/Domain/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  MyQiita
+//
+//  Created by Naoto Kaneko on 1/15/16.
+//  Copyright © 2016 Naoto Kaneko. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    let title: String
+    
+    init(title: String) {
+        self.title = title
+    }
+}
